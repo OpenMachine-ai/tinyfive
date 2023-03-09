1835a68ddb4689388ca9af94d974d503d7c02430
Nils Graef <nilsgraef@Nilss-Air.attlocal.net>
2023-03-08 21:26:17 -0800

    more layers

--- mobilenet_v1_0.25.py
+++ mobilenet_v1_0.25.py
@@ -10,7 +10,7 @@ from machine import machine
 from layers import *
 
 np.random.seed(5)  # fix seed for reproducible results
-m = machine(mem_size=4000000)  # instantiate RISC-V machine with 4MB of memory
+m = machine(mem_size=6000000)  # instantiate RISC-V machine with 6MB of memory
 # TODO: reduce to 500KB once we use branches to reduce image size
 
 #-------------------------------------------------------------------------------
@@ -100,6 +100,7 @@ print(l14.numpy().shape)
 
 # TODOs:
 #  - replace above by keras sequence (or import a reference model from HuggingFace or Keras)
+#  - use for-loops (for i range(30)) for these 29 layers to clean up the code
 #  - add the remaining layers 15 to 29
 #  - add ReLU
 #  - add biases
@@ -123,20 +124,38 @@ w13_base = w12.size * 4 + w12_base
 w14_base = w13.size * 4 + w13_base
 
 # TODO: optimize the memory footprint of the activations
-a1_base = w14.size * 4        + w14_base
-a2_base = inp.size * 4        + a1_base
-a3_base = l1.numpy().size * 4 + a2_base
-a4_base = l2.numpy().size * 4 + a3_base
-a5_base = l3.numpy().size * 4 + a4_base
+a1_base  = w14.size * 4         + w14_base
+a2_base  = inp.size * 4         + a1_base
+a3_base  = l1.numpy().size * 4  + a2_base
+a4_base  = l2.numpy().size * 4  + a3_base
+a5_base  = l3.numpy().size * 4  + a4_base
+a6_base  = l4.numpy().size * 4  + a5_base
+a7_base  = l5.numpy().size * 4  + a6_base
+a8_base  = l6.numpy().size * 4  + a7_base
+a9_base  = l7.numpy().size * 4  + a8_base
+a10_base = l8.numpy().size * 4  + a9_base
+a11_base = l9.numpy().size * 4  + a10_base
+a12_base = l10.numpy().size * 4 + a11_base
+a13_base = l11.numpy().size * 4 + a12_base
+a14_base = l12.numpy().size * 4 + a13_base
 # TODO: add more layers and update below
-out_base   = l4.numpy().size * 4 + a5_base
-code_start = l5.numpy().size * 4 + out_base
+out_base   = l13.numpy().size * 4 + a14_base
+code_start = l14.numpy().size * 4 + out_base
 
-m.write_f32_vec(np.transpose(w1, axes=[3, 0, 1, 2]).flatten(), w1_base)
-
-m.write_f32_vec(np.transpose(w2, axes=[2, 0, 1]).flatten(), w2_base)
-
-m.write_f32_vec(w3.flatten(), w3_base)
+m.write_f32_vec(np.transpose( w1, axes=[3, 0, 1, 2]).flatten(), w1_base)
+m.write_f32_vec(np.transpose( w2, axes=[2, 0, 1]).flatten(), w2_base)
+m.write_f32_vec(              w3.flatten(),                  w3_base)
+m.write_f32_vec(np.transpose( w4, axes=[2, 0, 1]).flatten(), w4_base)
+m.write_f32_vec(              w5.flatten(),                  w5_base)
+m.write_f32_vec(np.transpose( w6, axes=[2, 0, 1]).flatten(), w6_base)
+m.write_f32_vec(              w7.flatten(),                  w7_base)
+m.write_f32_vec(np.transpose( w8, axes=[2, 0, 1]).flatten(), w8_base)
+m.write_f32_vec(              w9.flatten(),                  w9_base)
+m.write_f32_vec(np.transpose(w10, axes=[2, 0, 1]).flatten(), w10_base)
+m.write_f32_vec(             w11.flatten(),                  w11_base)
+m.write_f32_vec(np.transpose(w12, axes=[2, 0, 1]).flatten(), w12_base)
+m.write_f32_vec(             w13.flatten(),                  w13_base)
+m.write_f32_vec(np.transpose(w14, axes=[2, 0, 1]).flatten(), w14_base)
 
 m.write_f32_vec(inp.flatten(), a1_base)
 
@@ -193,4 +212,57 @@ l3_asm = m.read_f32_vec(y_base, size=R*R*F).reshape(R, R, F)
 l3_ref = l3.numpy().reshape(R, R, F)
 m.print_rel_err(l3_asm, l3_ref)
 
-# TODO: add more layers here
+#-------------------------------------------------------------------------------
+# layer 4: Depthwise Conv2D 3x3, 16 channels, 48x48 resolution, stride=2
+#-------------------------------------------------------------------------------
+R = 48
+Q = R//2
+C = 16
+y_base = a5_base
+
+# TODO: remove below hack, temp hack to transpose the input activations
+l3_hack = np.transpose(l3_asm, axes=[2, 0, 1])
+m.write_f32_vec(l3_hack.flatten(), a4_base)
+
+dw_conv_3x3_stride2(m, C, R, a_base=a4_base, w_base=w4_base, y_base=y_base,
+                    out_chan_first=False)
+
+# compare results against keras
+l4_asm = m.read_f32_vec(y_base, size=Q*Q*C).reshape(Q, Q, C)
+l4_ref = l4.numpy().reshape(Q, Q, C)
+m.print_rel_err(l4_asm, l4_ref)
+
+#-------------------------------------------------------------------------------
+# layer 5: Conv2D 1x1, 16 in-channels, 32 out-channels, 24x24 resolution
+#-------------------------------------------------------------------------------
+R = 24
+C = 16
+F = 2*C
+y_base = a6_base
+
+conv_1x1(m, C, F, R, a_base=a5_base, w_base=w5_base, y_base=y_base,
+         code_start=code_start)
+
+# compare results against keras
+l5_asm = m.read_f32_vec(y_base, size=R*R*F).reshape(R, R, F)
+l5_ref = l5.numpy().reshape(R, R, F)
+m.print_rel_err(l5_asm, l5_ref)
+
+#-------------------------------------------------------------------------------
+# layer 6: Depthwise Conv2D 3x3, 32 channels, 24x24 resolution, stride=1
+#-------------------------------------------------------------------------------
+R = 24
+C = 32
+y_base = a7_base
+
+# TODO: remove below hack, temp hack to transpose the input activations
+l5_hack = np.transpose(l5_asm, axes=[2, 0, 1])
+m.write_f32_vec(l5_hack.flatten(), a6_base)
+
+dw_conv_3x3_stride1(m, C, R, a_base=a6_base, w_base=w6_base, y_base=y_base,
+                    out_chan_first=False)
+
+# compare results against keras
+l6_asm = m.read_f32_vec(y_base, size=R*R*C).reshape(R, R, C)
+l6_ref = l6.numpy().reshape(R, R, C)
+m.print_rel_err(l6_asm, l6_ref)
